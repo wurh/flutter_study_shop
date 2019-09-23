6f1b38f88f97d4e73e6759376076af9ea4d6fc44
wurh <ro87630872@126.com>
2019-09-23 20:34:33 +0800

    【04】底部导航栏切换效果
    【底部导航栏切换效果】

--- flutter_study_shop/lib/pages/index_page.dart
+++ flutter_study_shop/lib/pages/index_page.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_study_shop/pages/home_page.dart';
+import 'package:flutter_study_shop/pages/cart_page.dart';
+import 'package:flutter_study_shop/pages/category_page.dart';
+import 'package:flutter_study_shop/pages/member_page.dart';
 
-class IndexPage extends StatelessWidget {
-  const IndexPage({Key key}) : super(key: key);
+class IndexPage extends StatefulWidget {
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  final List<BottomNavigationBarItem> bottomTabs = [
+    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), title: Text('首页')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.search), title: Text('分类')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.shopping_cart), title: Text('购物车')),
+    BottomNavigationBarItem(
+        icon: Icon(CupertinoIcons.person), title: Text('会员中心')),
+  ];
+
+  final List tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage(),
+  ];
+
+  int currentIndex = 0;
+  var currentPage;
+
+  @override
+  void initState() {
+    currentPage = tabBodies[currentIndex];
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar:AppBar(
-        title: Text("百姓生活+"),
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: currentIndex,
+        items: bottomTabs,
+        onTap: (index){
+          setState(() {
+            currentIndex = index;
+            currentPage = tabBodies[currentIndex];
+          });
+        },
       ),
-       body: Center(
-         child: Text('百姓生活+'),
-       ),
-      ),
+      body: currentPage,
     );
   }
 }
